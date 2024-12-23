ce99fa5fc91377e0c593c2fe55755fd8c42f457c
Bruno Yamaguchi <tadashi.b@gmail.com>
2024-12-23 11:42:05 -0300

    `feat`: create `MissaoRepository`

---
+++ src/main/java/com/brunotadashi/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package com.brunotadashi.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
