e7d8c02993d77e06d33c57e1979be5daa8ac2583
Bruno Yamaguchi <tadashi.b@gmail.com>
2024-12-23 15:00:13 -0300

    `refactor`: modify id data type to primitive long

--- src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     private String nome;
 
