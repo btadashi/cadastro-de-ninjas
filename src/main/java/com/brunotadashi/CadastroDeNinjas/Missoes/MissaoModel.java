a097e97edd70b95abd90c3da6f8457200b23a4ee
Bruno Yamaguchi <tadashi.b@gmail.com>
2025-03-18 21:36:23 -0300

    `fix`: adds `@JsonIgnore` annotation to the `MissaoModel` to prevent the serialization loop error

--- src/main/java/com/brunotadashi/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/com/brunotadashi/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -1,6 +1,7 @@
 package com.brunotadashi.CadastroDeNinjas.Missoes;
 
 import com.brunotadashi.CadastroDeNinjas.Ninjas.NinjaModel;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,5 +25,7 @@ public class MissaoModel {
     private String dificuldade;
 
     @OneToMany(mappedBy = "missao")
+    // Ignora a serialização para a lista de ninjas.
+    @JsonIgnore
     private List<NinjaModel> ninjas;
 }
