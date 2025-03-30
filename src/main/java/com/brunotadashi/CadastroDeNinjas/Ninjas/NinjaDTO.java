7a5cbf243b3ca1ea90e2e1df056b6d5c0392fc05
Bruno Yamaguchi <tadashi.b@gmail.com>
2025-03-30 14:37:34 -0300

    `chore`: includes the DTO version from the entity and manually maps the entity x DTO paths

--- src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,19 @@
 package com.brunotadashi.CadastroDeNinjas.Ninjas;
 
+import com.brunotadashi.CadastroDeNinjas.Missoes.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private MissaoModel missao;
+    private String rank;
 }
