a3915532cdb228297b06eda9e443eba361501078
Bruno Yamaguchi <tadashi.b@gmail.com>
2025-04-06 21:39:03 -0300

    `refactor`: adds `ToString` annotation to the models

--- src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missao")
 @Table(name = "tb_ninja")
 public class NinjaModel {
     @Id
