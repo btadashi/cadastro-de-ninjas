024b0732274571c5d4915fe807309bfdbef4b33a
Bruno Yamaguchi <tadashi.b@gmail.com>
2025-04-13 11:48:25 -0300

    `feat`: adds Swagger dependency to the application for documenting controller routes

--- src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/brunotadashi/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,5 +1,9 @@
 package com.brunotadashi.CadastroDeNinjas.Ninjas;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +27,13 @@ public class NinjaController {
     }
 
     @PostMapping("/criar")
+    // A annotation `@Operation` permite atribuir um resumo e uma descrição do que a rota faz.
+    @Operation(summary = "Cria um novo ninja", description = "Esta rota cria um novo ninja e insere no banco de dados.")
+    // Podemos descrever as respostas que a rota pode fornecer.
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Ninja criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na criação do ninja")
+    })
     public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninjaDTO) {
         NinjaDTO newNinja = ninjaService.criarNinja(ninjaDTO);
 
@@ -30,6 +41,13 @@ public class NinjaController {
     }
 
     @GetMapping("/listar/{id}")
+    // A annotation `@Operation` permite atribuir um resumo e uma descrição do que a rota faz.
+    @Operation(summary = "Lista o ninja por Id", description = "Esta rota lista um ninja em específico através de seu Id.")
+    // Podemos descrever as respostas que a rota pode fornecer.
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "O ninja não foi encontrado")
+    })
     public ResponseEntity<?> listarNinjaPorId(@PathVariable Long id) {
         NinjaDTO ninja = ninjaService.listarNinjaPorId(id);
 
@@ -41,7 +59,19 @@ public class NinjaController {
     }
 
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> alterarNinja(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) {
+    // A annotation `@Operation` permite atribuir um resumo e uma descrição do que a rota faz.
+    @Operation(summary = "Altera o ninja por Id", description = "Esta rota altera os dados de um ninja por Id.")
+    // Podemos descrever as respostas que a rota pode fornecer.
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ninja alterado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "O ninja não foi encontrado. Não foi possível alterar")
+    })
+    public ResponseEntity<?> alterarNinja(
+            // Podemos documentar os parâmetros para dizer de onde eles vêm.
+            @Parameter(description = "O usuário manda no caminho da requisição")
+            @PathVariable Long id,
+            @Parameter(description = "O usuário manda os dados do ninja a ser atualizado no corpo da requisição")
+            @RequestBody NinjaDTO ninjaAtualizado) {
         NinjaDTO updatedNinja = ninjaService.atualizarNinja(id, ninjaAtualizado);
 
         if (updatedNinja != null) {
